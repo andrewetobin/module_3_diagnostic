38cb11e472d3975fcd63175d3d42aed5d62b85bc
andrewetobin <andrewetobin@gmail.com>
2018-10-17 09:25:50 -0600

    initial test

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -7,5 +7,27 @@ feature "User can visit root page" do
       expect(page).to have_content("AltFuelFinder")
       expect(page).to have_selector("input[value='Search by zip...']")
     end
+  end 
+  scenario "views 10 stations and stations info" do
+
+    # As a user
+    # When I visit "/"
+    visit "/"
+    # And I fill in the search form with 80203 (Note: Use the existing search form)
+    fill_in "Search by zip...50000", with: 80126
+
+    # And I click "Locate"
+    click_on "Locate"
+
+
+    # Then I should be on page "/search"
+
+    expect(current_path).to eq("/search")
+
+    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+
+    expect(page).to have_content("Renewable fuel station")
+    # And the stations should be limited to Electric and Propane
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
   end
 end
